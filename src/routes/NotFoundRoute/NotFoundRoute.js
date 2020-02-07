32ac331cb2f91dd10899d23a640a6595430487e3
Michael Weedman <michael.weedman@gmail.com>
2020-02-07 11:40:36 -0700

    added button to no-route page.

--- src/routes/NotFoundRoute/NotFoundRoute.js
+++ src/routes/NotFoundRoute/NotFoundRoute.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
 
 class NotFoundRoute extends Component {
   render() {
     return (
       <section className="viewport">
         <h2>404 - Page not found</h2>
-        <p>Try going back to your previous page.</p>
+        <p>Try going back to your previous page.
+        </p>
+        <Link to='/'><button>Go home!</button> </Link>
       </section>
     );
   }
